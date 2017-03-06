ea2ddc3076186b3c4ee7824ce68bb1da68071ace
ThuyNT13 <[01;31m[K"email"[m[K: null>
2017-03-06 16:56:51 -0600

    add alternative algorithms to Game of Life

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,3 +1,5 @@
+// how to implement event handling to applyRules function
+
 function play() {
   computeNextGen();
 
@@ -20,19 +22,86 @@ function computeNextGen() {
 
 function applyRules(row, col) {
   var numNeighbors = countNeighbors(row, col);
+
+  // // Game of Life
+  // if (grid[row][col] == 1) {
+  //   if (numNeighbors < 2) {
+  //     nextGrid[row][col]=0;
+  //   } else if (numNeighbors == 2 || numNeighbors == 3) {
+  //     nextGrid[row][col] = 1;
+  //   } else if (numNeighbors > 3) {
+  //     nextGrid[row][col] = 0;
+  //   }
+  // } else if (grid[row][col] == 0) {
+  //   if (numNeighbors == 3) {
+  //     nextGrid[row][col] = 1;
+  //   }
+  // }
+
+  // // MAZE generates a maze
+  // if (grid[row][col] == 1) {
+  //   if (numNeighbors > 0 && numNeighbors < 6) {
+  //     nextGrid[row][col] = 1;
+  //   } else {
+  //     nextGrid[row][col] = 0;
+  //   }
+  // } else {
+  //   if (numNeighbors == 3) {
+  //     nextGrid[row][col] = 1;
+  //   }
+  // }
+
+  // // NIGHT / DAY
+  // if (grid[row][col] == 1) {
+  //   if (numNeighbors == 3 || numNeighbors == 4 || numNeighbors > 5) {
+  //     nextGrid[row][col] = 1;
+  //   } else {
+  //     nextGrid[row][col] = 0;
+  //   }
+  // } else {
+  //   if (numNeighbors == 3 || numNeighbors > 5) {
+  //     nextGrid[row][col] = 1;
+  //   }
+  // }
+
+  // // WALLED CITY
+  // if (grid[row][col] == 1) {
+  //   if (numNeighbors > 3 && numNeighbors < 9) {
+  //     nextGrid[row][col] = 1;
+  //   } else {
+  //     nextGrid[row][col] = 0;
+  //   }
+  // } else {
+  //   if (numNeighbors > 1 && numNeighbors < 6) {
+  //     nextGrid[row][col] = 1;
+  //   }
+  // }
+
+  // Reverse Game of Life
   if (grid[row][col] == 1) {
-    if (numNeighbors < 2) {
-      nextGrid[row][col]=0;
-    } else if (numNeighbors == 2 || numNeighbors == 3) {
+    if (numNeighbors == 3) {
       nextGrid[row][col] = 1;
-    } else if (numNeighbors > 3) {
+    } else {
       nextGrid[row][col] = 0;
     }
-  } else if (grid[row][col] == 0) {
-    if (numNeighbors == 3) {
+  } else {
+    if (numNeighbors == 2 || numNeighbors == 3) {
       nextGrid[row][col] = 1;
     }
   }
+
+  // // Off and On
+  // if (grid[row][col] == 1) {
+  //   if (numNeighbors < 3 || numNeighbors == 4) {
+  //     nextGrid[row][col] = 1;
+  //   } else {
+  //     nextGrid[row][col] = 0;
+  //   }
+  // } else {
+  //   if (numNeighbors < 4 || numNeighbors == 5) {
+  //     nextGrid[row][col] = 1;
+  //   }
+  // }
 }
 
 function countNeighbors(row, col) {
